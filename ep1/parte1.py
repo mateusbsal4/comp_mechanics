e8fba798028de300730598032007abfdbdf5ffa2
pedro-fuoco <pedrofuoco6@gmail.com>
2023-05-29 14:34:59 -0300

    PARTE1: interface de terminal e bug fix do Y0

--- ep1/parte1.py
+++ ep1/parte1.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 from numba import njit
 
 
@@ -22,27 +21,27 @@ Fn = m_e*(w_e**2)*r
 
 @njit
 def d1(t, V):
-  w = 2*np.pi*(V/L)  
   if t>2:
     return 0
+  w = 2*np.pi*(V/L)  
   return A*(1-np.cos(w*t))
 @njit
 def d2(t, V):
-  w = 2*np.pi*(V/L)  
   if t>2:
     return 0
+  w = 2*np.pi*(V/L)  
   return A*(1+np.cos(w*t))
 @njit
 def d1_dot(t, V):
-  w = 2*np.pi*(V/L)  
   if t>2:
     return 0
+  w = 2*np.pi*(V/L)  
   return A*w*np.sin(w*t)
 @njit
 def d2_dot(t, V): 
-  w = 2*np.pi*(V/L)  
   if t>2:
     return 0
+  w = 2*np.pi*(V/L)  
   return -A*w*np.sin(w*t)
 
 @njit
@@ -72,7 +71,7 @@ def rk4_solver(Y0, t0, tf, h, k, c, V):
     return Y
 
 
-def rk4_plotter(h, V, k1, k2, c1, c2, scale_x, scale_theta, scale_xdot, scale_thetadot, scale_xddot, scale_thetaddot, Y0 = np.array([0, 0.09, 0, 0]), t0 = 0, tf = 4, n =1000):
+def rk4_plotter(h, V, k1, k2, c1, c2, scale_x, scale_theta, scale_xdot, scale_thetadot, scale_xddot, scale_thetaddot, Y0, t0 = 0, tf = 4, n =1000):
   t_sampled = np.linspace(t0, tf, n)
   Y = np.zeros((n,4))
   Y_dd = np.zeros((n, 2))
@@ -83,12 +82,14 @@ def rk4_plotter(h, V, k1, k2, c1, c2, scale_x, scale_theta, scale_xdot, scale_th
     u = Y[i][2]
     v = Y[i][3]
     Y_dd[i] = np.array([((-(k1+k2)*x+(k1*a-k2*b)*theta-(c1+c2)*u+(c1*a-c2*b)*v+k1*d1(t_sampled[i], V)+k2*d2(t_sampled[i], V)+c1*d1_dot(t_sampled[i], V)+c2*d2_dot(t_sampled[i], V)+Fn*np.sin(w_e*t_sampled[i]))/M), (((k1*a-k2*b)*x-(k1*a**2+k2*b**2)*theta+(c1*a-c2*b)*u-(c1*a**2+c2*b**2)*v-k1*a*d1(t_sampled[i], V)+k2*b*d2(t_sampled[i], V)-c1*a*d1_dot(t_sampled[i], V)+c2*b*d2_dot(t_sampled[i], V)-Fn*(e*np.sin(w_e*t_sampled[i])+f*np.cos(w_e*t_sampled[i])))/Ic)])
+  if (np.isnan(Y).any()):
+     print("Overflow no vetor resultante")
   Z = np.transpose(Y)
   Y_dd = np.transpose(Y_dd)
   fig, ax = plt.subplots(figsize=(10,10))
   ax.plot(t_sampled, scale_x*Z[0], label=r'$x(t) \times$' + "{}".format(scale_x))  
   ax.plot(t_sampled, scale_xdot*Z[2], label=r'$\dot{x}(t) \times$' + "{}".format(scale_xdot))  
-  #ax.plot(t_sampled, scale_xddot*Y_dd[0], label=r'$\ddot{x}(t) \times$' + "{}".format(scale_xddot))  
+  ax.plot(t_sampled, scale_xddot*Y_dd[0], label=r'$\ddot{x}(t) \times$' + "{}".format(scale_xddot))  
   ax.set_xlabel('t')  
   ax.set_ylabel('f(t)')  
   ax.set_title("Evolução temporal em x (h = {}, V = {}, k1=k2={}, c1=c2={})".format(h, V*3.6, k1, c1))  
@@ -103,14 +104,12 @@ def rk4_plotter(h, V, k1, k2, c1, c2, scale_x, scale_theta, scale_xdot, scale_th
   ax.set_title("Evolução temporal em theta (h = {}, V = {}, k1=k2={}, c1=c2={})".format(h, V*3.6, k1, c1))  
   ax.legend() 
 
-
   plt.show()
 
-
 def main():
-    h_s = 0.004    #small
-    h_m = 0.04    #medium
-    h_l = 0.4     #large
+    h_s = 0.002    #small
+    h_m = 0.004    #medium
+    h_l = 0.02     #large
     V1 = 50/3.6
     V2 = 30/3.6
     V3 = 70/3.6
@@ -124,25 +123,38 @@ def main():
     c_b3  = 2.5*10**5
 
 
+    print("Item a")
+    print("Mostrando resultados para o passo pequeno h = 0.002s")
+    rk4_plotter(h_s, V1, k_a, k_a, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))
+    print("Mostrando resultados para o passo medio h = 0.004s")
+    rk4_plotter(h_m, V1, k_a, k_a, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))
+    print("Mostrando resultados para o passo grande h = 0.02s")
+    rk4_plotter(h_l, V1, k_a, k_a, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))
 
-    #Plotting
-    #item a
-    #rk4_plotter(h_l, V1, k_a, k_a, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001)
-    #rk4_plotter(h_m, V1, k_a, k_a, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001)
-    #rk4_plotter(h_s, V1, k_a, k_a, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001)
+    print("\n")
 
-    ##item b
-    ##i)
-    #rk4_plotter(h_s, V2, k_a, k_a, c_a, c_a, 100, 100, 1, 1, 0.001, 0.001)
-    #rk4_plotter(h_s, V3, k_a, k_a, c_a, c_a, 10, 10, 1, 0.1, 0.001, 0.001)  
-    ##ii)
-    #rk4_plotter(h_s, V1, k_a, k_a, c_b1, c_b1, 10, 10, 0.1, 0.1, 0.001, 0.001)
-    #rk4_plotter(h_s, V1, k_a, k_a, c_b2, c_b2, 100, 100, 1, 0.1, 0.001, 0.001)
-    #rk4_plotter(h_s, V1, k_a, k_a, c_b3, c_b3, 10, 10, 0.1, 0.1, 0.001, 0.001)  
-    ##iii)
-    #rk4_plotter(h_s, V1, k_b1, k_b1, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001)
-    #rk4_plotter(h_s, V1, k_b2, k_b2, c_a, c_a, 100, 10, 1, 1, 0.01, 0.001)
-    rk4_plotter(h_s, V1, k_b3, k_b3, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001)
+    print("Item b")
+    print("i)")
+    print("Mostrando resultados para V = 30km/h") 
+    rk4_plotter(h_s, V2, k_a, k_a, c_a, c_a, 100, 100, 1, 1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))
+    print("Mostrando resultados para V = 70km/h") 
+    rk4_plotter(h_s, V3, k_a, k_a, c_a, c_a, 10, 10, 1, 0.1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))  
+    
+    print("ii)")
+    print("Mostrando resultados para c = 1000 kg/s")
+    rk4_plotter(h_s, V1, k_a, k_a, c_b1, c_b1, 10, 10, 0.1, 0.1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))
+    print("Mostrando resultados para c = 50000 kg/s")
+    rk4_plotter(h_s, V1, k_a, k_a, c_b2, c_b2, 100, 100, 1, 0.1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))
+    print("Mostrando resultados para c = 250000 kg/s") 
+    rk4_plotter(h_s, V1, k_a, k_a, c_b3, c_b3, 10, 10, 0.1, 0.1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))  
+    
+    print("iii)")
+    print("Mostrando resultados para k = 2000 N/m")
+    rk4_plotter(h_s, V1, k_b1, k_b1, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))
+    print("Mostrando resultados para k = 5000000 N/m")
+    rk4_plotter(h_s, V1, k_b2, k_b2, c_a, c_a, 100, 10, 1, 1, 0.01, 0.001, np.array([0, 0.09, 0, 0]))
+    print("Mostrando resultados para k = 700000000 N/m")
+    rk4_plotter(h_s, V1, k_b3, k_b3, c_a, c_a, 10, 10, 0.1, 0.1, 0.001, 0.001, np.array([0, 0.09, 0, 0]))
 
 
 if __name__ == "__main__":
